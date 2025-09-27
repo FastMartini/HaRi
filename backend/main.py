cb81ea4d5258cb706981bb6748253f703e9a8234
Mattjava <77417435+Mattjava@users.noreply.github.com>
2025-09-27 18:01:52 -0400

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -9,19 +9,5 @@ origins = [
 
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-@app.get("/document")
-async def documentation():
-    return {"message": "Document Response"}
 
-@app.get("/qa")
-async def qa():
-    return {"message": "QA response"}
-
-@app.get("/training")
-async def training():
-    return {"message": "Training response"}
-
-@app.get("loop")
-async def loop():
-    return {"message": "Loop response"}
 
