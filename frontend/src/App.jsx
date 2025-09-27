a4775ac938993eb6089bd962c6df8c0600e7945b
JustinCardenas0425 <jcardenas0425@gmail.com>
2025-09-27 07:52:55 -0400

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,14 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
 
          <Navbar />
          <Hero />  
