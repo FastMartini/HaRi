6961ee7579b29bfee13278076c52234f60c88626
JustinCardenas0425 <jcardenas0425@gmail.com>
2025-09-27 05:11:04 -0400

    added homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
   
          <Navbar />
+         <Hero />
       
       <p className="read-the-docs">
        Powered by Google ADK + A2A
---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import "./Hero.css";
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <h1 className="title">Meet HaRi, Your AI onboarding assistant</h1>
+            <p className="text">Seamlessly oboard, train, and support your team with a touch of intelligence.</p>
+            <button className="get-started">Get started right now</button>
+        </div>
+    );
+}
