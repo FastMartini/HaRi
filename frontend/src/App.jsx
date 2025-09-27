4e6a84b70433edd707a13f6435befa6a25c012b6
JustinCardenas0425 <jcardenas0425@gmail.com>
2025-09-27 07:51:32 -0400

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,23 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
          <Navbar />
          <Hero />  
         
       <p className="read-the-docs">
        Powered by Google ADK + A2A
       </p>
+
+
     </>
-  );
+  )
 }
+
 export default App
